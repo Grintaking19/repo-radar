81e0dc31183eb7c7c9cf7170c0d6b0db69d22d4d
Grintaking19 <omarbadrh.ob@gmail.com>
2026-09-20 15:29:58 +0300

    bug(deps): missing types/react and types/react-dom dependencies in package/ui

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { ThemeProvider, CssBaseline } from "@mui/material";
-import { lightTheme, darkTheme } from "@repo-radar/ui";
+import { darkTheme } from "@repo-radar/ui";
 import { store } from "./app/store.ts";
 import "./index.css";
 import App from "./App.tsx";
